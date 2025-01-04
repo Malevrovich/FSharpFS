28257c62567b0024ea956ca1189fcfdd655104af
malevrovich <dddccc20000@mail.ru>
2025-01-05 00:43:19 +0300

    added HelloFUSE fs

---
+++ src/FSharpFS/HelloFUSE.fs
@@ -0,0 +1,72 @@
+module FSFS
+
+open Tmds.Fuse
+open System.Text
+open Tmds.Linux
+open FSharp.Span.Utils
+open type Tmds.Linux.LibC
+
+type HelloFPFileSystem() =
+    inherit FuseFileSystemBase()
+
+    let helloFilePath = "/hello" |> Encoding.UTF8.GetBytes
+
+    let helloFileContent = "Hello World!" |> Encoding.UTF8.GetBytes
+    let rootPath = "/" |> Encoding.UTF8.GetBytes
+
+    override this.SupportsMultiThreading: bool = true
+
+    override this.GetAttr(path: System.ReadOnlySpan<byte>, stat: byref<Tmds.Linux.stat>, fiRef: FuseFileInfoRef) : int =
+        match path with
+        | root when Span.sequenceEqual path (span rootPath) ->
+            stat.st_mode <- (S_IFDIR ||| (0b111_111_111 |> uint16 |> mode_t.op_Implicit)) // rwxr-xr-x
+            stat.st_nlink <- 2 |> uint64 |> nlink_t.op_Implicit
+            0
+        | hello when Span.sequenceEqual path (span helloFilePath) ->
+            stat.st_mode <- (S_IFREG ||| (0b100_100_100 |> uint16 |> mode_t.op_Implicit)) // r--r--r--
+            stat.st_nlink <- 1 |> uint64 |> nlink_t.op_Implicit
+            stat.st_size <- helloFileContent.Length |> off_t.op_Implicit
+            0
+        | _ -> -ENOENT
+
+    override this.Open(path: System.ReadOnlySpan<byte>, fi: byref<FuseFileInfo>) : int =
+        match path with
+        | hello when Span.sequenceEqual path (span helloFilePath) ->
+            if fi.flags &&& O_ACCMODE <> O_RDONLY then //
+                -EACCES
+            else
+                0
+        | _ -> -ENOENT
+
+    override this.Read
+        (path: System.ReadOnlySpan<byte>, offset: uint64, buffer: System.Span<byte>, fi: byref<FuseFileInfo>)
+        : int =
+        match path with
+        | hello when Span.sequenceEqual path (span helloFilePath) ->
+            if offset > uint64 helloFileContent.Length then
+                0
+            else
+                let start = int offset
+                let len = min buffer.Length (helloFileContent.Length - start)
+
+                (span helloFileContent).Slice(start, len).CopyTo(buffer)
+                len
+        | _ -> -ENOENT
+
+    override this.ReadDir
+        (
+            path: System.ReadOnlySpan<byte>,
+            offset: uint64,
+            flags: ReadDirFlags,
+            content: DirectoryContent,
+            fi: byref<FuseFileInfo>
+        ) : int =
+        match path with
+        | root when Span.sequenceEqual path (span rootPath) ->
+            let dirs = [ "."; ".."; "hello" ]
+
+            for i in 0 .. dirs.Length - 1 do
+                content.AddEntry(dirs[i])
+
+            0
+        | _ -> -ENOENT
--- src/FSharpFS/Program.fs
+++ src/FSharpFS/Program.fs
@@ -1,2 +1,25 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open Tmds.Fuse
+open FSFS
+open System.IO
+
+if not (Fuse.CheckDependencies()) then
+    printfn "%s" Fuse.InstallationInstructions
+else
+    let fs = new HelloFPFileSystem() :> IFuseFileSystem
+
+    let mountPoint = "/tmp/HelloFS"
+
+    printfn "Mounting HelloFilesystem at %s" mountPoint
+
+    Fuse.LazyUnmount(mountPoint)
+    Directory.CreateDirectory(mountPoint) |> ignore
+
+    try
+        using (Fuse.Mount(mountPoint, fs)) (fun mount ->
+            printfn "Mount completed"
+            mount.WaitForUnmountAsync() |> Async.AwaitTask |> Async.RunSynchronously)
+    with :? FuseException as ex ->
+        printfn "Fuse throw an exception: %A" ex
+        printfn "Try unmounting the file system by executing:"
+        printfn "fuser -kM %s" mountPoint
+        printfn "sudo umount -f %s" mountPoint
